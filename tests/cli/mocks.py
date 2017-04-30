ea3be184d00b8e1a7a86f8cddd9d3d47938ae360
McKay <mckaykward@gmail.com>
2017-04-29 23:32:32 -0500

    move logging to ExperimentClient and adapt tests

--- tests/cli/mocks.py
+++ tests/cli/mocks.py
@@ -11,24 +11,6 @@ def mock_exp(exp_id):
     return Experiment()
 
 
-def mock_running_exp(exp_id):
-    class Experiment:
-        id = exp_id
-        state = 'running'
-        name = 'running experiment'
-        task_instances = []
-    return Experiment()
-
-
-def mock_queued_exp(exp_id):
-    class Experiment:
-        id = exp_id
-        state = 'queued'
-        name = 'queued experiment'
-        task_instances = []
-    return Experiment()
-
-
 def mock_experiment_config():
     return ExperimentConfig(name="name", family_id="family_id")
 
