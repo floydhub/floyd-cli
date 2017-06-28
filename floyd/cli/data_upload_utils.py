348aed41f105dd38343e8e6d29ceb00639083bd0
Qingping Hou <dave2008713@gmail.com>
2017-06-28 14:41:20 -0700

    fix: also remove temporary upload directory when aborting uploads

--- floyd/cli/data_upload_utils.py
+++ floyd/cli/data_upload_utils.py
@@ -123,7 +123,7 @@ def complete_upload(data_config):
 
 def abort_previous_upload(data_config):
     try:
-        os.remove(data_config.tarball_path)
+        rmtree(os.path.dirname(data_config.tarball_path))
     except (OSError, TypeError):
         pass
 
