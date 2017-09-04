5df18a17bb3006b1f14d7e93a487fc24348082a5
Naren Thiagarajan <narenst@gmail.com>
2017-09-04 09:57:12 -0700

    Regression: Handle invalid token error
    Now that we are returning json for invalid token error the CLI
    was throwing this error:
    
    ```
      File "/Users/naren/Code/floyd/floyd-cli/floyd/model/user.py", line 14,
      in make_user
          return User(**data)
        TypeError: __init__() missing 4 required positional arguments:
        'uid', 'username', 'email', and 'type'
    ```
    
    This was because the login client was not using the same base web client
    as the other clients.

--- floyd/client/auth.py
+++ floyd/client/auth.py
@@ -1,5 +1,3 @@
-import requests
-
 import floyd
 from floyd.exceptions import AuthenticationException
 from floyd.client.base import FloydHttpClient
@@ -14,9 +12,9 @@ class AuthClient(FloydHttpClient):
         self.base_url = "{}/api/v1/user/".format(floyd.floyd_host)
 
     def get_user(self, access_token):
-        response = requests.get(self.base_url,
-                                headers={"Authorization": "Bearer {}".format(access_token)})
         try:
+            response = self.requests.get(self.base_url,
+                                         headers={"Authorization": "Bearer {}".format(access_token)})
             user_dict = response.json()
         except Exception:
             raise AuthenticationException("Invalid Token.\nSee http://docs.floydhub.com/faqs/authentication/ for help")
