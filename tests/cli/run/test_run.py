7167850cf83ec0dec484de7170d5458bf0e6c87b
Qingping Hou <dave2008713@gmail.com>
2018-02-05 16:22:45 -0800

    add data mount test

--- tests/cli/run/test_run.py
+++ tests/cli/run/test_run.py
@@ -126,3 +126,28 @@ class TestExperimentRun(unittest.TestCase):
 
         result = self.runner.invoke(run, ['--env', 'foo', 'ls'])
         assert_exit_code(result, 0)
+
+    @patch('floyd.cli.run.DataClient.get')
+    @patch('floyd.model.access_token.assert_token_not_expired')
+    @patch('floyd.cli.run.EnvClient.get_all', return_value={'cpu': {'foo': 'foo', 'bar': 'bar'}})
+    @patch('floyd.cli.run.AuthConfigManager.get_access_token', side_effect=mock_access_token)
+    @patch('floyd.cli.run.ExperimentConfigManager.get_config', side_effect=mock_experiment_config)
+    @patch('floyd.cli.run.ExperimentConfigManager.set_config')
+    @patch('floyd.cli.run.ModuleClient.create', return_value='module_id')
+    @patch('floyd.cli.run.ExperimentClient')
+    @patch('floyd.cli.run.ProjectClient.exists', return_value=True)
+    def test_mount_dataset_without_version(self,
+                                           exists,
+                                           expt_client,
+                                           create_module,
+                                           set_config,
+                                           get_config,
+                                           get_access_token,
+                                           get_all_env,
+                                           assert_token_not_expired,
+                                           data_client_get):
+        """
+        CLI should fail if more than one --env is passed
+        """
+        result = self.runner.invoke(run, ['--env', 'foo', '--data', 'foo/datasets/bar', 'ls'])
+        assert_exit_code(result, 0)
