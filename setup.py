b0431c053c8c0d855cb907f11a1f283e83fe48f0
Justin Zhang <twojustin@gmail.com>
2018-06-15 09:36:19 -0700

    bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 from setuptools import find_packages, setup
 
 project = "floyd-cli"
-version = "0.11.0"
+version = "0.11.1"
 
 with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
     long_description = readme.read()
